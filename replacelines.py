f28c0810b16766d302e7128780c139fbe751a802
g40244 <gustav.uhia@gmail.com>
2018-01-02 07:21:19 +0000

    hoge

--- replacelines.py
+++ replacelines.py
@@ -1,3 +1,4 @@
+
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
@@ -14,54 +15,51 @@ def main():
             state=dict(required=True, choices=['present', 'absent'])
         )
     )
-    path = module.param['path']
-    prev = module.param['prev']
-    current = module.param['current']
-    state = module.param['state']
+    path = module.params['path']
+    prev = module.params['prev']
+    current = module.params['current']
+    state = module.params['state']
+
+    if not re.findall('\n$', current):
+        current += '\n'
+
+    if not re.findall('\n$', prev):
+        prev += '\n'
 
     if not os.path.exists(path):
         module.fail_json(msg='[Error] file '+ path +' is not found.')
-        
+
     elif os.path.isdir(path):
         module.fail_json(msg='[Error] '+ path +' is directory.')
-        
+
     elif os.path.islink(path):
         module.fail_json(msg='[Error] '+ path +' is link.')
 
     line = open(path).read()
-    
+
     if state == 'present':
-        if not re.findall('\n$', current, re.MULTILINE):
-            current += '\n'
-    
-        if re.findall(current, line, re.MULTILINE):
+        if current in line:
             module.exit_json(changed=False)
-    
-        prev = re.findall(prev, line, re.MULTILINE)
-        
-        if not prev:
-            module.fail_json(msg='[Error] prev lines "'+ prev +'" is not found.')
-        
-        elif len(prev) != 1:
-            module.fail_json(msg='[Error] prev lines "'+ prev +'" is multiple found.')
-    
-        prev = prev[0] + '\n'
-        line = re.sub(prev, current, line)
-    
+
+        if not prev in line:
+            module.fail_json(msg='[Error] prev lines is not found.')
+
+        line = line.replace(prev, current)
+
     elif state == 'absent':
-        if not re.findall('\n$', prev, re.MULTILINE):
-            prev += '\n'
-        
-        if not re.findall('\n$', current, re.MULTILINE):
-            current += '\n'
-        
-        if not re.findall(current, line, re.MULTILINE):
-            if re.findall(prev, line, re.MULTILINE):
+        if not current in line:
+            if prev in line:
                 module.exit_json(changed=False)
-            
-        line = re.sub(current, prev, line)
-    
+            else:
+                module.fail_json(msg='[Error] current lines is not found.')
+
+        line = line.replace(current, prev)
+
+    f = open(path, 'w')
+    f.write(line)
+    f.close()
+
     module.exit_json(changed=True)
-        
+
 if __name__ == '__main__':
     main()
